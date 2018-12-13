62b60a6c88099b8655e3e340845f988b2ee3c7fe
matt2tech <mlipsey@basecampcodingacademy.org>
2018-12-13 16:01:09 -0600

    Added tables BlogPost and Comments

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    title = models.TextField()
+    body = models.TextField()
+    author = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    cover_image_url = models.TextField()
+
+class Comments(models.Model):
+    title = models.TextField()
+    body = models.TextField()
+    author = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField()
+    blog_post = models.ForeignKey(BlogPost)

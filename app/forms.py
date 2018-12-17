45eada75e9807563b3e223dbf5ec7432ca544f1d
matt2tech <mlipsey@basecampcodingacademy.org>
2018-12-17 15:58:17 -0600

    Added form validation for class CommentCreate

--- app/forms.py
+++ app/forms.py
@@ -7,4 +7,9 @@ class BCreateForm(Form):
     body = forms.CharField(label='Body')
     cover_image = forms.URLField(label="Cover Image")
 
-
+class CCreateForm(Form):
+    title = forms.CharField(label='Title')
+    author = forms.CharField(label='Author')
+    body = forms.CharField(label='Body')
+    rating = forms.IntegerField(min_value=1, max_value=5)
+    

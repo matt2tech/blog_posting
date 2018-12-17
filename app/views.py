0959a10dce8a6cdecd372b60b6f48baa0836bd16
matt2tech <mlipsey@basecampcodingacademy.org>
2018-12-17 11:03:07 -0600

    Added class BlogCreate

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+# from . import forms
+from app import models
 
 # Create your views here.
 class Home(View):
     def get(self, request):
         return render(request, 'home.html', {'Blogs': models.BlogPost.objects.all()})
+
+class BlogCreate(View):
+    def get(self, request):
+        return render(request, 'blog_create.html', {'forms': forms.BCreateForms()})
